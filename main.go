3b62a0c5f310342d039e0dd558bee43837bbf681
Stephen Stefanatos <stephenstef456@gmail.com>
2024-04-16 23:16:43 -0400

    Queue logic

--- main.go
+++ main.go
@@ -5,19 +5,62 @@ import (
 	"io"
 	"net/http"
 	"golang.org/x/net/websocket"
+  "encoding/json"
 )
 
+const (
+  MAX_PLAYERS = 2
+
+  EASY = 1
+  MEDIUM = 2
+  HARD = 3
+
+  EASY_TIME = 15
+  MEDIUM_TIME = 25
+  HARD_TIME = 35
+
+  EASY_POINTS = 10 
+  MEDIUM_POINTS = 50
+  HARD_POINTS = 100
+)
+
+type Question struct {
+  Description string
+  Answer string
+  Difficulty int
+  Category string
+  Points int
+  Time int
+}
+
+type Ranked_Set struct {
+  Player1 User
+  Player2 User
+  Question Question 
+}
+
 type Server struct {
-  clients []*websocket.Conn
+  Clients []User
+  Queue []User
+  Current_Games []Ranked_Set
 }
 
-func (server *Server) handle_conn(socket *websocket.Conn) {
+type User struct {
+  ID string
+  Username string
+  Email string
+  Conn *websocket.Conn
+}
+
+func (server *Server) Open_conn(socket *websocket.Conn) {
   fmt.Println("Connection established", socket.RemoteAddr())
-  server.clients = append(server.clients, socket)
-  server.listen(socket)
+  user := User{ ID: "1", Username: "guest", Email: "", Conn: socket }
+  server.Clients = append(server.Clients, user)
+  fmt.Println("Number of clients:", server.Clients, len(server.Clients))
+  server.Listen(socket)
 }
 
-func (server *Server) listen(socket *websocket.Conn) {
+func (server *Server) Listen(socket *websocket.Conn) {
   buf := make([]byte, 1024)
   for {
     n, err := socket.Read(buf)
@@ -28,26 +71,80 @@ func (server *Server) listen(socket *websocket.Conn) {
       fmt.Println("Read error:", err)
       continue
     }
-    msg := buf[:n]
-    fmt.Println("Received message:", string(msg))
-    server.broadcast(msg)
-    socket.Write([]byte("Thanks for the message"))
+    bytes := buf[:n]
+    var data map[string]interface{}
+    err = json.Unmarshal(bytes, &data)
+    fmt.Println("Recieved Data:", data)
+    server.Handle_data(data, socket)
+    if err != nil {
+      fmt.Println("Error unmarshalling JSON:", err)
+      continue
+    }
+    //server.Broadcast(msg)
+    //socket.Write([]byte("Thanks for the message"))
+  }
+  server.Close_conn(socket)
+}
+
+func (server *Server) Handle_data(data map[string]interface{}, socket *websocket.Conn) {
+  if data["type"] == "login" {
+    username := data["username"].(string)
+    email := data["email"].(string)
+    user := User{ ID: "uuid", Username: username, Email: email, Conn: socket }
+    found := false
+    for i, client := range server.Clients {
+      if client.Conn == socket {
+        server.Clients[i] = user
+        found = true
+        break
+      }
+    }
+    if !found {
+      fmt.Println("User not found")
+      socket.Write([]byte("User not found"))
+    }
   }
 }
 
-func (server *Server) broadcast(msg []byte) {
-  for _, socket := range server.clients {
-    _, err := socket.Write(msg)
+func (server *Server) Close_conn(socket *websocket.Conn) {
+  fmt.Println("Closing connection", socket.RemoteAddr())
+  for i, client := range server.Clients {
+    if client.Conn == socket {
+      server.Clients = append(server.Clients[:i], server.Clients[i+1:]...)
+      break
+    }
+  }
+  fmt.Println("Number of clients:", server.Clients, len(server.Clients))
+  socket.Close()
+}
+
+func (server *Server) Broadcast(msg []byte) {
+  for _, user := range server.Clients {
+    _, err := user.Conn.Write(msg)
     if err != nil {
       fmt.Println(err)
     }
   }
 }
 
+func (server *Server) Remove_from_queue() {
+  server.Queue = server.Queue[1:]
+}
+
+func (server *Server) Add_to_queue(socket *websocket.Conn) bool {
+  if(len(server.Queue) < MAX_PLAYERS) {
+    return true
+  } else {
+    server.Remove_from_queue()
+    return false
+  }
+}
+
 func main() {
   fmt.Println("Starting server on port 8080")
-  server := Server{ clients: make([]*websocket.Conn, 0), }
-  http.Handle("/ws", websocket.Handler(server.handle_conn))
+  server := Server{ Clients: []User{}, Queue: []User{}, Current_Games: []Ranked_Set{} }
+
+  http.Handle("/ws", websocket.Handler(server.Open_conn))
 
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     template := template.Must(template.ParseFiles("index.html"))
@@ -55,6 +152,21 @@ func main() {
     template.Execute(w, data)
   })
 
+  http.HandleFunc("/find-match/", func (w http.ResponseWriter, r *http.Request) {
+
+  })
+
   http.ListenAndServe(":8080", nil)
+}
+
+func get_question() *Question {
+  return &Question{
+    Description: "What is the capital of France?",
+    Answer: "Paris",
+    Difficulty: EASY,
+    Category: "Geography",
+    Points: EASY_POINTS,
+    Time: EASY_TIME,
+  }
 }
 
